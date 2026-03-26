7e6e66527ac6fac4af90f6cbd96bc10a7154b693
p78g <IsabelleRose6691ttt@outlook.com>
2026-03-26 21:04:59 +0000

    Create color.js

---
+++ Common/color.js
@@ -0,0 +1,6 @@
+export const red = (text) => `\x1b[31m${text}\x1b[0m`;
+export const yellow = (text) => `\x1b[33m${text}\x1b[0m`;
+export const orange = (text) => `\u001b[38;5;208m${text}\u001b[0m`;
+export const green = (text) => `\x1b[32m${text}\x1b[0m`;
+export const purple = (text) => `\u001b[38;5;129m${text}\u001b[0m`;
+export const bold = (text) => `\x1b[1m${text}\x1b[0m`;
