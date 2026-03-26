9920aa81b9bf9e4287737e95ab525849747c994f
p78g <IsabelleRose6691ttt@outlook.com>
2026-03-26 21:08:48 +0000

    Create file.js

---
+++ legacy/scraper/file.js
@@ -0,0 +1,5 @@
+// find a chunk that is similar to this
+// https://ac.blooket.com/play-frontend/a7fe5cdd6905a5c507686db9923391900044b628/_next/static/chunks/3ae5402b-6471d6f29e77e48b.js
+// https://archive.ph/IBEVH
+// then paste it here and run "bun scraper/grabber"
+// this will populate legacy/blooks.js with all new blooks
